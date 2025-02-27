7a1ff3d5ace8dcf8962f4e18d0a71b9112134bea
Christian Tarifeno <christiantarifer@gmail.com>
2025-02-26 19:23:10 -0500

    Added winner to tic tae toe game

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,13 +1,74 @@
+import { useState } from 'react'
 import './App.css'
 
-// const TURNS = {
-//   X: 'x',
-//   O: 'o'
-// }
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
 
-const board = Array(9).fill(null)
+const Square = ( { children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => { 
+    updateBoard(index)
+  }
+
+  return(
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
 
 function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null))
+  
+  console.log(board);
+
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) // null No winner - false a tie
+
+  const checkWinner = (boardToCheck) => {
+    for ( const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+  }
+
+  const updateBoard = (index) => {
+    if(board[index] || winner ) return
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner){
+      alert(`El ganador es ${newWinner}`)
+      setWinner(newWinner)
+    }
+  }
 
   return (
     <main className="board">
@@ -16,15 +77,21 @@ function App() {
         {
           board.map( (_, index) => {
             return(
-              <div className="cell" key={index}>
-                <span className='cell__content'>
-                  {index}
-                </span>
-              </div>
+              <Square 
+                key={index}
+                index={index}
+                updateBoard={updateBoard}> 
+                {board[index]}
+              </Square>
             )
           })
         }
       </section>
+
+      <div className="turn">
+        <Square isSelected={turn === TURNS.X} >{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </div>
     </main>
   )
 }
